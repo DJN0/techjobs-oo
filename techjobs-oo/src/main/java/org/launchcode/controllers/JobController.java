45a0c36a738f540d84d38b159e291c5973eb6a01
DJN0 <connorzong@gmail.com>
2017-12-18 17:18:27 -0600

    Finished todos

---
+++ techjobs-oo/src/main/java/org/launchcode/controllers/JobController.java
@@ -0,0 +1,58 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Job;
+import org.launchcode.models.data.JobData;
+import org.launchcode.models.forms.JobForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value = "job")
+public class JobController {
+
+    private JobData jobData = JobData.getInstance();
+
+    // The detail display for a given Job at URLs like /job?id=17
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, int id) {
+
+        // TODO #1 - get the Job with the given ID and pass it into the view
+        model.addAttribute(jobData.findById(id));
+
+        return "job-detail";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new JobForm());
+        return "new-job";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+
+        // TODO #6 - Validate the JobForm model, and if valid, create a
+        // new Job and add it to the jobData data store. Then
+        // redirect to the job detail view for the new Job.
+
+        if (errors.hasErrors()) {
+            return "new-job";
+        }
+
+        Job newJob = new Job(jobForm.getName(),
+                jobData.findById(jobForm.getEmployerId()).getEmployer(),
+                jobData.findById(jobForm.getLocationId()).getLocation(),
+                jobData.findById(jobForm.getPositionTypeId()).getPositionType(),
+                jobData.findById(jobForm.getCoreCompetencyId()).getCoreCompetency());
+
+        jobData.add(newJob);
+        model.addAttribute(newJob);
+        return "job-detail";
+
+    }
+}
